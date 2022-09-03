c2dfb448d74520a3bd7162ffa048c9592d384bac
Anis Heit <73469982+puthiboi@users.noreply.github.com>
2022-09-03 13:02:42 +0200

    Update credentials.py

--- Code/credentials.py
+++ Code/credentials.py
@@ -1,6 +1,6 @@
 ####input your credentials here
 
-consumer_key = 'iUccfbzK0lbN4eHJu6V5Xwx8E'
-consumer_secret = '8rcQWEz0yQk5xYGeLQYIJcguDjzryKsOv7GPk9SJwI9fTz10hG'
-access_token = '1265951102958272513-pQtRB29RopTkHBaNL7VpTUuD7q6QMx'
-access_token_secret = 'RhtFqNKbbgBg45xp21uwWk2iT2S5XtRlk3zXZ3j6Hpo03'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''

4d5338872e507d2225054056db661f7600547d7f
Anis Heit <73469982+puthiboi@users.noreply.github.com>
2022-09-03 04:01:11 -0700

    initial commit

---
+++ Code/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# This is a shell script that sets up the environment for the project
+
+pip install geopy
+pip install numpy
+pip install pandas
+pip install gmaps
+pip install tweepy
+pip install pyvis
